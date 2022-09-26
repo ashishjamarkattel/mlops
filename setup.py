f8186b4ec6c686b0bf75c54f8645ab83624dfd2a
ashishjamarkattel <ashishjamarkattel123@gmail.com>
2022-09-26 19:29:16 +0545

    mlops_initial_commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from pathlib import Path 
+from setuptools import find_namespace_packages, setup 
+
+## load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+
+setup(
+    name="tagifai",
+    version=0.1,
+    description="classification machine learning projects",
+    author="ashihs",
+    author_email="ashishjamarkattel123@gmail.com",
+    python_requires=">3.7",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages]
+)
